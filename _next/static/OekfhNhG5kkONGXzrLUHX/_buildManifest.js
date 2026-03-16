b7033b0dfe2a1408b306e2b3642574faad6e3a52
shivamu1818 <shivam.gazaniainfotech.com>
2026-03-16 20:21:12 +0530

    Updates

--- _next/static/OekfhNhG5kkONGXzrLUHX/_buildManifest.js
+++ _next/static/OekfhNhG5kkONGXzrLUHX/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/your-repo-name//_next/:path+",
-        "destination": "/your-repo-name/_next/:path+"
+        "source": "/shivamportfolio//_next/:path+",
+        "destination": "/shivamportfolio/_next/:path+"
       }
     ],
     "fallback": []
